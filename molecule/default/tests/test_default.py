e8ee566d62f724014fa0db7852ef45709a180317
darkwizard242 <am900820@gmail.com>
2020-11-28 15:47:07 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,17 +6,33 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'logrotate'
+PACKAGE_BINARY = '/usr/sbin/logrotate'
+
+
 def test_logrotate_package_installed(host):
+    """
+    Tests if logrotate package is installed.
+    """
     assert host.package("logrotate").is_installed
 
 
 def test_logrotate_binary_exists(host):
-    assert host.file('/usr/sbin/logrotate').exists
+    """
+    Tests if logrotate binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_logrotate_binary_file(host):
-    assert host.file('/usr/sbin/logrotate').is_file
+    """
+    Tests if logrotate binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_logrotate_binary_which(host):
-    assert host.check_output('which logrotate') == '/usr/sbin/logrotate'
+    """
+    Tests the output to confirm logrotate's binary location.
+    """
+    assert host.check_output('which logrotate') == PACKAGE_BINARY
